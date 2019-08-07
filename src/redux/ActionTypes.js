149ac421fc32b22d1dbd550ef4073f21e1bee077
Lucas Salmons <lucsalmons@gmail.com>
2019-08-06 23:01:18 -0700

    pull request clean up

--- src/redux/ActionTypes.js
+++
@@ -1,3 +0,0 @@
-export const ADD_TASK = 'ADD_TASK';
-export const TASKS_FAILED = 'TASKS_FAILED';
-export const ADD_TASKS = 'ADD_TASKS';
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,5 +1,7 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { createForms } from 'react-redux-form';
+import { StartingTask } from './tasks';
+import thunk from 'redux-thunk';
 
 export const ConfigureStore = () => {
     const store = createStore(
